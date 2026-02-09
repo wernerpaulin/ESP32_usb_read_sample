934b1dbdf85e1eef95c371ad8145a09e39ba9169
Werner Paulin <72560449+wernerpaulin@users.noreply.github.com>
2026-02-09 15:36:36 +0100

    LED blink test working

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 
+// LED pins
+constexpr int RED_LED    = 25;
+constexpr int GREEN_LED  = 27;
+constexpr int YELLOW_LED = 26;
+
 // put function declarations here:
 int myFunction(int, int);
 
@@ -7,11 +12,22 @@ void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
   Serial.begin(9600);
+
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Ready to receive data...");
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(1000);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
   delay(1000);
 }
 
