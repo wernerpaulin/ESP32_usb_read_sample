312f99f0653b8bb4ae74e89c2bc8a58aefc04be1
Werner Paulin <72560449+wernerpaulin@users.noreply.github.com>
2026-03-20 14:50:45 +0100

    update with LED blinking and new servo pin

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,41 +1,28 @@
+#include <HardwareSerial.h>
 #include <Arduino.h>
 #include <ESP32Servo.h>
-
 
 Servo sorterServo;
 
 // ===== CONFIG =====
-const int SERVO_PIN = 13;
+const int SERVO_PIN = 19;
+const int LED_PIN = 18;        // Test-LED Pin
 
-// Change these angles to match your conveyor layout
-// avoid 0 and 180 to avoid hitting mechanical limits of the servo. Adjust as needed for your setup.
-const int IDLE_ANGLE = 10;     // 
-const int BIN_1_ANGLE = 30;    // Straight / default
-const int BIN_2_ANGLE = 90;   // Center
-const int BIN_3_ANGLE = 140;  // Right
+const int IDLE_ANGLE = 10;     
+const int BIN_1_ANGLE = 50;    
+const int BIN_2_ANGLE = 110;   
+const int BIN_3_ANGLE = 155;
+
+const int SERVO_AUTO_RETURN_TIME_MS = 6000;
 // ==================
-
 
 unsigned long moveTime = 0;
 bool waitingToReturn = false;
 
-
-// LED pins
-constexpr int RED_LED    = 25;
-constexpr int GREEN_LED  = 26;
-constexpr int YELLOW_LED = 27;
-
-void lightLedOn(int pin) {
-  digitalWrite(pin, HIGH);
-}
-
-
-void lightLedAllOff() {
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(YELLOW_LED, LOW);
-}
-
+// Led Blinking
+unsigned long previousMillisLed = 0;
+const long ledInterval = 500;  
+bool ledState = LOW;
 
 void moveServoTo(int angle) {
   sorterServo.write(angle);
@@ -44,52 +31,50 @@ void moveServoTo(int angle) {
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(1000);
 
-  pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(YELLOW_LED, OUTPUT);
+  // init LED
+  pinMode(LED_PIN, OUTPUT);
 
+  // Servo Setup
+  ESP32PWM::allocateTimer(0);
   sorterServo.setPeriodHertz(50);
   sorterServo.attach(SERVO_PIN, 500, 2400);
 
-  // Start straight
-  sorterServo.write(BIN_1_ANGLE);
-
-  Serial.println("ESP32 ready");
+  // Start position
+  sorterServo.write(IDLE_ANGLE);
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillisLed >= ledInterval) {
+    previousMillisLed = currentMillis;
+    ledState = !ledState;
+    digitalWrite(LED_PIN, ledState);
+  }
+
+  // serial commands
   if (Serial.available()) {
     String cmd = Serial.readStringUntil('\n');
-    cmd.trim();  // remove \r, spaces, etc.
+    cmd.trim(); 
 
     if (cmd == "BIN_1") {
-      lightLedOn(RED_LED);
       moveServoTo(BIN_1_ANGLE);
     } 
     else if (cmd == "BIN_2") {
-      lightLedOn(YELLOW_LED); 
       moveServoTo(BIN_2_ANGLE);
     } 
     else if (cmd == "BIN_3") {
-      lightLedOn(GREEN_LED);
       moveServoTo(BIN_3_ANGLE);
     } 
-    else {
-      Serial.print("Unknown command: ");
-      Serial.println(cmd);
+    else if (cmd != "") {
     }
   }
 
-  // ===== AUTO RETURN AFTER 4s =====
-  if (waitingToReturn && millis() - moveTime >= 4000) {
+  // --- AUTO RETURN NACH 6s ---
+  if (waitingToReturn && currentMillis - moveTime >= SERVO_AUTO_RETURN_TIME_MS) {
     waitingToReturn = false;
     sorterServo.write(IDLE_ANGLE);
-    lightLedAllOff();
-    Serial.println("Returned to idle angle");
-  }  
+  }
 }
-
-
